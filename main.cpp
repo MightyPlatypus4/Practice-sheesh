1cfbe8d2a53703f46557a1bc47dd4bd62d5ef48b
MightyPlatypus4 <125356462+MightyPlatypus4@users.noreply.github.com>
2023-03-21 13:48:19 +0300

    Task 1 i am dead

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Triangle.h"
+int main()
+{
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Enter a,b,c for triangle n " << i + 1 << " with space " << std::endl;
+        std::cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            std::cout << "Nope, try again" << std::endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        std::cout << "Perymeter: " << mas[i].perimetr() << std::endl;
+        std::cout << "Square: " << mas[i].square() << std::endl;
+    }
+    return 0;
+}
